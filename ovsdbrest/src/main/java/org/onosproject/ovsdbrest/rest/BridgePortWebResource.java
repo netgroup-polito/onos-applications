0f72938ff1b28f548d7bcadd70fc1d2d43b17b64
gabrielecastellano <gabry.c92x@gmail.com>
2016-08-18 18:09:56 +0200

    removed unused imports

--- ovsdbrest/src/main/java/org/onosproject/ovsdbrest/rest/BridgePortWebResource.java
+++ ovsdbrest/src/main/java/org/onosproject/ovsdbrest/rest/BridgePortWebResource.java
@@ -13,8 +13,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
