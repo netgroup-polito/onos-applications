33dcc80748ca152b6e9b7cec333dfbd0a08276e9
gabrielecastellano <gabry.c92x@gmail.com>
2016-12-19 18:18:22 +0100

    added missing parameters in docstring

--- ovsdbrest/src/main/java/org/onosproject/ovsdbrest/OvsdbRestService.java
+++ ovsdbrest/src/main/java/org/onosproject/ovsdbrest/OvsdbRestService.java
@@ -9,58 +9,66 @@ public interface OvsdbRestService {
 
     /**
      * Creates a new bridge.
-     * @param bridgeName bridge name
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
      */
     void createBridge(IpAddress ovsdbAddress, String bridgeName) throws OvsdbRestException.OvsdbDeviceException,
             OvsdbRestException.BridgeAlreadyExistsException;
 
     /**
      * Deletes a bridge.
-     * @param bridgeName bridge identifier
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
      */
     void deleteBridge(IpAddress ovsdbAddress, String bridgeName) throws OvsdbRestException.OvsdbDeviceException,
             OvsdbRestException.BridgeNotFoundException;
 
     /**
-     * Add a port to a bridge.
-     * @param bridgeName bridge identifier
-     * @param portName port name
+     * Adds a port to a bridge.
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
+     * @param portName the name of the port to attach to the bridge
      */
     void addPort(IpAddress ovsdbAddress, String bridgeName, String portName)
             throws OvsdbRestException.OvsdbDeviceException, OvsdbRestException.BridgeNotFoundException;
 
     /**
-     * Remove a port from a bridge.
-     * @param bridgeName bridge identifier
-     * @param portName port name
+     * Removes a port from a bridge.
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
+     * @param portName the name of the port to remove from the bridge
      */
     void removePort(IpAddress ovsdbAddress, String bridgeName, String portName)
             throws OvsdbRestException.OvsdbDeviceException, OvsdbRestException.BridgeNotFoundException;
 
     /**
-     * Add a patch port to a bridge setting it as peer of an other port.
-     * @param bridgeName bridge identifier
-     * @param portName port name
-     * @param patchPeer peer port name
+     * Adds a patch port to a bridge setting it as peer of an other port.
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
+     * @param portName the port name
+     * @param patchPeer the name of the peer port
      */
     void createPatchPeerPort(IpAddress ovsdbAddress, String bridgeName, String portName, String patchPeer)
             throws OvsdbRestException.OvsdbDeviceException;
 
     /**
-     * Create a gre tunnel from a bridge port to a remote destination.
-     * @param bridgeName bridge identifier
-     * @param portName port name
-     * @param remoteIp remote end point of gre tunnel
-     * @param key the tunnel key
+     * Creates a GRE tunnel from a bridge to a remote destination.
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
+     * @param portName the name of the new GRE port
+     * @param localIp local end point of the GRE tunnel
+     * @param remoteIp remote end point of GRE tunnel
+     * @param key the tunnel key, should represent a 32 bit hexadecimal number
      */
     void createGreTunnel(IpAddress ovsdbAddress, String bridgeName, String portName, IpAddress localIp,
                          IpAddress remoteIp, String key)
             throws OvsdbRestException.OvsdbDeviceException, OvsdbRestException.BridgeNotFoundException;
 
     /**
-     * Delete a gre tunnel given the port name.
-     * @param bridgeName bridge identifier
-     * @param portName port name
+     * Deletes a GRE tunnel given the port name.
+     * @param ovsdbAddress the ovsdb IP address
+     * @param bridgeName the bridge identifier
+     * @param portName the name of the GRE
      */
     void deleteGreTunnel(IpAddress ovsdbAddress, String bridgeName, String portName)
             throws OvsdbRestException.OvsdbDeviceException;
