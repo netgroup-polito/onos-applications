88ffff7b3543cca83b8de84c6760be447f595caf
lara <lara@ubuntu>
2017-05-23 09:42:21 -0700

    properties file

--- model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
+++ model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
@@ -176,6 +176,7 @@ public class StateListenerNew extends Thread{
         try{
             Properties prop = new Properties();
             InputStream propFile = loader.getResourceAsStream("configuration/appProperties.properties");
+            prop.load(propFile);
             log.info("appId "+prop.getProperty("appId"));
             log.info("baseUri "+prop.getProperty("baseUri"));
             log.info("eventsUri "+prop.getProperty("eventsUri"));
