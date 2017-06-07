140f1cc5da21d200127a2f5b25f58b7189f2f4c7
lara <lara@ubuntu>
2017-06-07 04:12:44 -0700

    check the complex objects in the maps

--- model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
+++ model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
@@ -1841,10 +1841,14 @@ public class StateListenerNew extends Thread{
                     fYang = YangToJava.get("root/"+tillHereJava+"/{key}/"+fieldName);
                 log.info("fYang "+fYang);
                 if(fYang!=null){
-                fYang = fYang.substring(fYang.lastIndexOf("/")+1);
-                log.info("Ovvero "+fYang);
-                if(indexObj.has(fYang) && !indexObj.get(fYang).equals(fields[i].get(obj)))
-                    return false;
+                    fYang = fYang.substring(fYang.lastIndexOf("/")+1);
+                    log.info("Ovvero "+fYang);
+                    log.info("oggetto nella chiave "+fields[i].get(obj));
+                    log.info("oggetto nell'index "+indexObj.get(fYang));
+                    if(indexObj.has(fYang) && !indexObj.get(fYang).equals(fields[i].get(obj))){
+                        log.info("****torno comunque FALSE****");
+                        return false;
+                    }
                 }
             }
             return true;
