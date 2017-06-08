076e642c79488e9a825854fc2b31d7d90fc47d98
lara <lara@ubuntu>
2017-06-08 04:02:52 -0700

    installing the nat table rules

--- model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/FlowIdentifier.java
+++ model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/FlowIdentifier.java
@@ -5,6 +5,7 @@
  */
 package org.onosproject.model.based.configurable.nat;
 
+import org.onlab.packet.Ip4Address;
 import org.onlab.packet.IpAddress;
 
 /**
@@ -12,17 +13,17 @@ import org.onlab.packet.IpAddress;
  * @author lara
  */
 public class FlowIdentifier {
-    public IpAddress srcIp;
+    public Ip4Address srcIp;
     public Short srcPort;
-    public IpAddress dstIp;
+    public Ip4Address dstIp;
     public Short dstPort;
     public byte protocol;
     
-    public IpAddress getSrcIp() {
+    public Ip4Address getSrcIp() {
         return srcIp;
     }
 
-    public void setSrcIp(IpAddress srcIp) {
+    public void setSrcIp(Ip4Address srcIp) {
         this.srcIp = srcIp;
     }
 
@@ -34,11 +35,11 @@ public class FlowIdentifier {
         this.srcPort = srcPort;
     }
 
-    public IpAddress getDstIp() {
+    public Ip4Address getDstIp() {
         return dstIp;
     }
 
-    public void setDstIp(IpAddress dstIp) {
+    public void setDstIp(Ip4Address dstIp) {
         this.dstIp = dstIp;
     }
 
