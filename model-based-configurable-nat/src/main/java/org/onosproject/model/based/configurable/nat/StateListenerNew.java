536b57b2c7df185a1f7a227cfbf39748c33efc35
lara <lara@ubuntu>
2017-06-09 03:36:11 -0700

    differentiation app not found and variable not found

--- model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
+++ model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
@@ -1991,7 +1991,7 @@ public class StateListenerNew extends Thread{
                     log.info("oggetto nella chiave "+fields[i].get(obj));
                     log.info("oggetto nell'index "+indexObj.get(fYang));
                     log.info("indexObje ha il campo? "+indexObj.has(fYang));
-                    log.info("The value in indexObj "+indexObj.get(fYang));
+                    log.info("The value in indexObj "+indexObj.get(fYang).textValue());
                     log.info("And in obj "+fields[i].get(obj));
                     if(indexObj.has(fYang) && !indexObj.get(fYang).toString().equals(fields[i].get(obj).toString())){
                         log.info("****torno comunque FALSE****");
