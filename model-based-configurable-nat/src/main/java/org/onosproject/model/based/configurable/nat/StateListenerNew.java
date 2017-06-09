a17d88c42d83bc8a39de830064a0763cc3d720a8
lara <lara@ubuntu>
2017-06-09 04:22:54 -0700

    nat rules

--- model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
+++ model-based-configurable-nat/src/main/java/org/onosproject/model/based/configurable/nat/StateListenerNew.java
@@ -1435,7 +1435,7 @@ public class StateListenerNew extends Thread{
                                 natPort = entry.get("newPort").shortValue();
                             log.info("p3 "+natPort);
                             if(entry.has("proto"))
-                                proto = entry.get("proto").binaryValue()[0];
+                                proto = Byte.parseByte(entry.get("proto").textValue());
                             log.info("input address "+inIp);
                             log.info("input port "+inPort);
                             log.info("output address "+outIp);
